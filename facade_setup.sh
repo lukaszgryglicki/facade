a27686eaeabf4d44123df56cb41157e535672bd5
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-07 08:23:04 +0200

    lukaszgryglicki-import-settings-attempt

--- facade_setup.sh
+++ facade_setup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /facade/utilities/
 python automatic_setup.py c yes facade facade localhost facade no yes admin exampleemail@domain.com admin admin
+python import_gitdm_configs.py -a ../cncf-config/aliases -e ../cncf-config/email-map -e ../cncf-config/domain-map
