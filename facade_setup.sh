3756b1236a8d121cdb4e553ebb6aa647f7d5f04a
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-07 14:22:56 +0200

    lukaszgryglicki-updated-facade-setup.sh

--- facade_setup.sh
+++ facade_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 cd /facade/utilities/
 echo "DB schema setup"
-python automatic_setup.py c yes facade facade localhost facade no yes admin exampleemail@domain.com admin admin
+python automatic_setup.py c yes facade facade localhost facade no yes admin exampleemail@domain.com admin admin || exit 1
 echo "Import CNCF/gitdm settings"
-python import_gitdm_configs.py -a ../cncf-config/aliases -e ../cncf-config/email-map -e ../cncf-config/domain-map -e ../cncf-config/group-map
+python import_gitdm_configs.py -a ../cncf-config/aliases -e ../cncf-config/email-map -e ../cncf-config/domain-map -e ../cncf-config/group-map || exit 2
 mkdir /opt/facade && mkdir /opt/facade/git-trees/ && chown facade /opt/facade/git-trees/
+git config merge.renameLimit 100000
+git config diff.renameLimit 100000
 cd /facade/
 echo "Restoring Facade projects/Repos setup"
 xz -d dumps/facade_configured.sql.xz && mysql -ufacade -pfacade facade < dumps/facade_configured.sql
