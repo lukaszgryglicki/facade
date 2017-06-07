9b4d56dd25e6c62e7e31cb4b94e185c47322ca4c
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-07 15:14:55 +0200

    lukaszgryglicki-first-results-and-populated-db-dump

--- facade_setup.sh
+++ facade_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 cd /facade/utilities/
 echo "DB schema setup"
-python automatic_setup.py c yes facade facade localhost facade no yes admin exampleemail@domain.com admin admin || exit 1
+#python automatic_setup.py c yes facade facade localhost facade no yes admin exampleemail@domain.com admin admin || exit 1
 echo "Import CNCF/gitdm settings"
-python import_gitdm_configs.py -a ../cncf-config/aliases -e ../cncf-config/email-map -e ../cncf-config/domain-map -e ../cncf-config/group-map || exit 2
+#python import_gitdm_configs.py -a ../cncf-config/aliases -e ../cncf-config/email-map -e ../cncf-config/domain-map -e ../cncf-config/group-map || exit 2
 mkdir /opt/facade && mkdir /opt/facade/git-trees/ && chown facade /opt/facade/git-trees/
 git config merge.renameLimit 100000
 git config diff.renameLimit 100000
 cd /facade/
 echo "Restoring Facade projects/Repos setup"
-xz -d dumps/facade_configured.sql.xz && mysql -ufacade -pfacade facade < dumps/facade_configured.sql
+# xz -d dumps/facade_configured.sql.xz && mysql -ufacade -pfacade facade < dumps/facade_configured.sql && rm -f dumps/facade_configured.sql
+# xz -d dumps/facade_populated.sql.xz && mysql -ufacade -pfacade facade < dumps/facade_populated.sql && rm -f dumps/facade_populated.sql
 echo "Setup complete"
