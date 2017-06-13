588b29f58d2daab1df99a0a3cf2825b7cd121eb9
lukaszgryglicki <lukaszgryglicki@o2.pl>
2017-06-13 11:40:54 +0200

    lukaszgryglicki-intermediate-new-run-imported-stackalytics-json

--- run_facade.sh
+++ run_facade.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd /facade/utilities/
 echo "Running Facade analysis"
+git config merge.renameLimit 100000
+git config diff.renameLimit 100000
 python facade-worker.py
 echo "Facade complete"
